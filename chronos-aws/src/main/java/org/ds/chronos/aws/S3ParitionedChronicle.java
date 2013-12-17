70ddff2c618a75d976c892d54ba79ff6cb6607af
Dan Simpson <dan.simpson@gmail.com>
2013-12-16 16:19:30 -0800

    Add gzip constructor arg to partitioned s3 chronicle

--- chronos-aws/src/main/java/org/ds/chronos/aws/S3ParitionedChronicle.java
+++ chronos-aws/src/main/java/org/ds/chronos/aws/S3ParitionedChronicle.java
@@ -10,16 +10,23 @@ public class S3ParitionedChronicle extends PartitionedChronicle {
 
 	final AmazonS3Client client;
 	final String bucket;
+	final boolean gzip;
 
 	public S3ParitionedChronicle(AmazonS3Client client, String bucket, String keyPrefix, PartitionPeriod period) {
+		this(client, bucket, keyPrefix, period, false);
+	}
+
+	public S3ParitionedChronicle(AmazonS3Client client, String bucket, String keyPrefix, PartitionPeriod period,
+	    boolean gzip) {
 		super(keyPrefix, period);
 		this.client = client;
 		this.bucket = bucket;
+		this.gzip = gzip;
 	}
 
 	@Override
-  protected Chronicle getPartition(String key) {
-	  return new S3Chronicle(client, bucket, key);
-  }
+	protected Chronicle getPartition(String key) {
+		return new S3Chronicle(client, bucket, key, gzip);
+	}
 
 }
