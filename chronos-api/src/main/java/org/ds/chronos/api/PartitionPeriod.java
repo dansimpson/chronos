a1ce7cbe875ffce2c52e9fe48bd33cc038d0ce79
Dan Simpson <dan.simpson@gmail.com>
2014-01-10 12:07:19 -0800

    Fix bug PartitionPeriod.getParitionKeys(range) method which missed the last key for some ranges

--- chronos-api/src/main/java/org/ds/chronos/api/PartitionPeriod.java
+++ chronos-api/src/main/java/org/ds/chronos/api/PartitionPeriod.java
@@ -15,82 +15,87 @@ import java.util.TimeZone;
  */
 public enum PartitionPeriod {
 
-  DAY(Calendar.DAY_OF_MONTH), MONTH(Calendar.MONTH), YEAR(Calendar.YEAR);
+	DAY(Calendar.DAY_OF_MONTH),
+	MONTH(Calendar.MONTH),
+	YEAR(Calendar.YEAR);
 
-  private int calendarField;
+	private int calendarField;
 
-  private PartitionPeriod(int calendarField) {
-    this.calendarField = calendarField;
-  }
+	private PartitionPeriod(int calendarField) {
+		this.calendarField = calendarField;
+	}
 
-  public final int getCalendarField() {
-    return calendarField;
-  }
+	public final int getCalendarField() {
+		return calendarField;
+	}
 
-  public final String getPeriodKey(String key, long timestamp) {
-    return getPeriodKey(key, getCalendar(timestamp));
-  }
+	public final String getPeriodKey(String key, long timestamp) {
+		return getPeriodKey(key, getCalendar(timestamp));
+	}
 
-  public final String getPeriodKey(String key, Calendar date) {
+	public final String getPeriodKey(String key, Calendar date) {
 
-    switch (this) {
-    case DAY:
-      return String.format("%s-%04d-%02d-%02d", key, date.get(Calendar.YEAR),
-          date.get(Calendar.MONTH) + 1, date.get(Calendar.DAY_OF_MONTH));
-    case MONTH:
-      return String.format("%s-%04d-%02d", key, date.get(Calendar.YEAR),
-          date.get(Calendar.MONTH) + 1);
-    case YEAR:
-      return String.format("%s-%04d", key, date.get(Calendar.YEAR));
-    default:
-      return null;
-    }
-  }
+		switch (this) {
+		case DAY:
+			return String.format("%s-%04d-%02d-%02d", key, date.get(Calendar.YEAR), date.get(Calendar.MONTH) + 1,
+			    date.get(Calendar.DAY_OF_MONTH));
+		case MONTH:
+			return String.format("%s-%04d-%02d", key, date.get(Calendar.YEAR), date.get(Calendar.MONTH) + 1);
+		case YEAR:
+			return String.format("%s-%04d", key, date.get(Calendar.YEAR));
+		default:
+			return null;
+		}
+	}
 
-  public final List<String> getPeriodKeys(String prefix, Date d1, Date d2) {
-    return getPeriodKeys(prefix, d1.getTime(), d2.getTime());
-  }
+	public final List<String> getPeriodKeys(String prefix, Date d1, Date d2) {
+		return getPeriodKeys(prefix, d1.getTime(), d2.getTime());
+	}
 
-  private static final TimeZone TZ = TimeZone.getTimeZone("UTC");
+	private static final TimeZone TZ = TimeZone.getTimeZone("UTC");
 
-  public final List<String> getPeriodKeys(String prefix, long t1, long t2) {
-    if (t1 > t2) {
-      List<String> keys = getPeriodKeys(prefix, t2, t1);
-      Collections.reverse(keys);
-      return keys;
-    }
+	public final List<String> getPeriodKeys(String prefix, long t1, long t2) {
+		if (t1 > t2) {
+			List<String> keys = getPeriodKeys(prefix, t2, t1);
+			Collections.reverse(keys);
+			return keys;
+		}
 
-    Calendar calendar = getCalendar(t1);
+		Calendar calendar = getCalendar(t1);
 
-    LinkedList<String> keys = new LinkedList<String>();
-    while (calendar.getTimeInMillis() <= t2) {
-      keys.add(getPeriodKey(prefix, calendar));
-      calendar.add(calendarField, 1);
-    }
-    return keys;
-  }
+		LinkedList<String> keys = new LinkedList<String>();
+		while (calendar.getTimeInMillis() <= t2) {
+			keys.add(getPeriodKey(prefix, calendar));
+			calendar.add(calendarField, 1);
+			clearCal(calendar);
+		}
+		return keys;
+	}
 
-  private Calendar getCalendar(long timestamp) {
-    Calendar calendar = Calendar.getInstance();
-    calendar.setTimeInMillis(timestamp);
-    calendar.set(Calendar.AM_PM, 0);
-    calendar.set(Calendar.HOUR, 0);
-    calendar.set(Calendar.MINUTE, 0);
-    calendar.set(Calendar.SECOND, 0);
-    calendar.set(Calendar.MILLISECOND, 0);
-    calendar.setTimeZone(TZ);
-    
-    // We need to fetch every key which could have data.  If we
-    // have a query that falls on the 8th, this will account for
-    // that, and make sure we get all keys
-    if(this == MONTH) {
-      calendar.set(Calendar.DAY_OF_MONTH, 1);
-    }
-    
-    if(this == YEAR) {
-      calendar.set(Calendar.MONTH, 0);
-    }
-    
-    return calendar;
-  }
+	private Calendar getCalendar(long timestamp) {
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTimeInMillis(timestamp);
+		calendar.setTimeZone(TZ);
+		clearCal(calendar);
+		return calendar;
+	}
+
+	private void clearCal(Calendar calendar) {
+		calendar.set(Calendar.AM_PM, 0);
+		calendar.set(Calendar.HOUR, 0);
+		calendar.set(Calendar.MINUTE, 0);
+		calendar.set(Calendar.SECOND, 0);
+		calendar.set(Calendar.MILLISECOND, 0);
+
+		// We need to fetch every key which could have data. If we
+		// have a query that falls on the 8th, this will account for
+		// that, and make sure we get all keys
+		if (this == MONTH || this == YEAR) {
+			calendar.set(Calendar.DAY_OF_MONTH, 1);
+		}
+
+		if (this == YEAR) {
+			calendar.set(Calendar.MONTH, 0);
+		}
+	}
 }
--- chronos-api/src/test/java/org/ds/chronos/api/PartitionPeriodTest.java
+++ chronos-api/src/test/java/org/ds/chronos/api/PartitionPeriodTest.java
@@ -2,113 +2,220 @@ package org.ds.chronos.api;
 
 import java.util.Calendar;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import junit.framework.Assert;
 
+import org.ds.chronos.api.chronicle.MemoryChronicle;
 import org.ds.chronos.support.TestBase;
+import org.ds.chronos.util.Duration;
 import org.junit.Test;
+
+import com.google.common.collect.Iterators;
 
 public class PartitionPeriodTest extends TestBase {
 
-  @Test
-  public void shouldGenerateRowKeysForDays() throws ChronosException {
-    PartitionPeriod period = PartitionPeriod.DAY;
+	@Test
+	public void shouldGenerateRowKeysForDays() throws ChronosException {
+		PartitionPeriod period = PartitionPeriod.DAY;
 
-    Calendar calendar = Calendar.getInstance();
-    calendar.set(2012, 0, 1);
-    Date d1 = calendar.getTime();
-    calendar.set(2012, 8, 1);
-    Date d2 = calendar.getTime();
+		Calendar calendar = Calendar.getInstance();
+		calendar.set(2012, 0, 1);
+		Date d1 = calendar.getTime();
+		calendar.set(2012, 8, 1);
+		Date d2 = calendar.getTime();
 
-    List<String> keys = period.getPeriodKeys("key", d1, d2);
+		List<String> keys = period.getPeriodKeys("key", d1, d2);
 
-    Assert.assertEquals(245, keys.size());
-    Assert.assertEquals("key-2012-01-01", keys.get(0));
-    Assert.assertEquals("key-2012-09-01", keys.get(keys.size() - 1));
-  }
+		Assert.assertEquals(245, keys.size());
+		Assert.assertEquals("key-2012-01-01", keys.get(0));
+		Assert.assertEquals("key-2012-09-01", keys.get(keys.size() - 1));
+	}
 
-  @Test
-  public void shouldWorkOnTightBoundaries() throws ChronosException {
-    PartitionPeriod period = PartitionPeriod.DAY;
+	@Test
+	public void shouldWorkOnTightBoundaries() throws ChronosException {
+		PartitionPeriod period = PartitionPeriod.DAY;
 
-    Calendar calendar = Calendar.getInstance();
-    calendar.set(2012, 0, 1, 23, 0, 0);
-    Date d1 = calendar.getTime();
-    calendar.set(2012, 0, 2, 1, 0, 0);
-    Date d2 = calendar.getTime();
+		Calendar calendar = Calendar.getInstance();
+		calendar.set(2012, 0, 1, 23, 0, 0);
+		Date d1 = calendar.getTime();
+		calendar.set(2012, 0, 2, 1, 0, 0);
+		Date d2 = calendar.getTime();
 
-    List<String> keys = period.getPeriodKeys("key", d1, d2);
+		List<String> keys = period.getPeriodKeys("key", d1, d2);
 
-    Assert.assertEquals(2, keys.size());
-    Assert.assertEquals("key-2012-01-01", keys.get(0));
-    Assert.assertEquals("key-2012-01-02", keys.get(keys.size() - 1));
-  }
+		Assert.assertEquals(2, keys.size());
+		Assert.assertEquals("key-2012-01-01", keys.get(0));
+		Assert.assertEquals("key-2012-01-02", keys.get(keys.size() - 1));
+	}
 
-  @Test
-  public void shouldWorkInReverse() throws ChronosException {
-    PartitionPeriod period = PartitionPeriod.DAY;
+	@Test
+	public void shouldWorkInReverse() throws ChronosException {
+		PartitionPeriod period = PartitionPeriod.DAY;
 
-    Calendar calendar = Calendar.getInstance();
-    calendar.set(2012, 0, 1, 23, 0, 0);
-    Date d1 = calendar.getTime();
-    calendar.set(2012, 0, 2, 1, 0, 0);
-    Date d2 = calendar.getTime();
+		Calendar calendar = Calendar.getInstance();
+		calendar.set(2012, 0, 1, 23, 0, 0);
+		Date d1 = calendar.getTime();
+		calendar.set(2012, 0, 2, 1, 0, 0);
+		Date d2 = calendar.getTime();
 
-    List<String> keys = period.getPeriodKeys("key", d2, d1);
+		List<String> keys = period.getPeriodKeys("key", d2, d1);
 
-    Assert.assertEquals(2, keys.size());
-    Assert.assertEquals("key-2012-01-02", keys.get(0));
-    Assert.assertEquals("key-2012-01-01", keys.get(keys.size() - 1));
-  }
+		Assert.assertEquals(2, keys.size());
+		Assert.assertEquals("key-2012-01-02", keys.get(0));
+		Assert.assertEquals("key-2012-01-01", keys.get(keys.size() - 1));
+	}
 
-  @Test
-  public void shouldGenerateRowKeysForMonths() throws ChronosException {
-    PartitionPeriod period = PartitionPeriod.MONTH;
+	@Test
+	public void shouldGenerateRowKeysForMonths() throws ChronosException {
+		PartitionPeriod period = PartitionPeriod.MONTH;
 
-    Calendar calendar = Calendar.getInstance();
-    calendar.set(2012, 0, 1);
-    Date d1 = calendar.getTime();
-    calendar.set(2012, 8, 1);
-    Date d2 = calendar.getTime();
+		Calendar calendar = Calendar.getInstance();
+		calendar.set(2012, 0, 1);
+		Date d1 = calendar.getTime();
+		calendar.set(2012, 8, 1);
+		Date d2 = calendar.getTime();
 
-    List<String> keys = period.getPeriodKeys("key", d1, d2);
+		List<String> keys = period.getPeriodKeys("key", d1, d2);
 
-    Assert.assertEquals(9, keys.size());
-    Assert.assertEquals("key-2012-01", keys.get(0));
-    Assert.assertEquals("key-2012-09", keys.get(keys.size() - 1));
-  }
+		Assert.assertEquals(9, keys.size());
+		Assert.assertEquals("key-2012-01", keys.get(0));
+		Assert.assertEquals("key-2012-09", keys.get(keys.size() - 1));
+	}
 
-  @Test
-  public void shouldGenerateRowKeysForYears() throws ChronosException {
-    PartitionPeriod period = PartitionPeriod.YEAR;
+	@Test
+	public void shouldGenerateRowKeysForYears() throws ChronosException {
+		PartitionPeriod period = PartitionPeriod.YEAR;
 
-    Calendar calendar = Calendar.getInstance();
-    calendar.set(2012, 0, 1);
-    Date d1 = calendar.getTime();
-    calendar.set(2013, 8, 1);
-    Date d2 = calendar.getTime();
+		Calendar calendar = Calendar.getInstance();
+		calendar.set(2012, 0, 1);
+		Date d1 = calendar.getTime();
+		calendar.set(2013, 8, 1);
+		Date d2 = calendar.getTime();
 
-    List<String> keys = period.getPeriodKeys("key", d1, d2);
+		List<String> keys = period.getPeriodKeys("key", d1, d2);
 
-    Assert.assertEquals(2, keys.size());
-    Assert.assertEquals("key-2012", keys.get(0));
-    Assert.assertEquals("key-2013", keys.get(1));
-  }
+		Assert.assertEquals(2, keys.size());
+		Assert.assertEquals("key-2012", keys.get(0));
+		Assert.assertEquals("key-2013", keys.get(1));
+	}
 
-  @Test
-  public void shouldGenerateRowKeyForYear() throws ChronosException {
-    PartitionPeriod period = PartitionPeriod.YEAR;
+	@Test
+	public void shouldGenerateRowKeyForYear() throws ChronosException {
+		PartitionPeriod period = PartitionPeriod.YEAR;
 
-    Calendar calendar = Calendar.getInstance();
-    calendar.set(2012, 0, 1);
-    Date d1 = calendar.getTime();
-    calendar.set(2012, 8, 1);
-    Date d2 = calendar.getTime();
-    List<String> keys = period.getPeriodKeys("key", d1, d2);
+		Calendar calendar = Calendar.getInstance();
+		calendar.set(2012, 0, 1);
+		Date d1 = calendar.getTime();
+		calendar.set(2012, 8, 1);
+		Date d2 = calendar.getTime();
+		List<String> keys = period.getPeriodKeys("key", d1, d2);
 
-    Assert.assertEquals(1, keys.size());
-    Assert.assertEquals("key-2012", keys.get(0));
-  }
+		Assert.assertEquals(1, keys.size());
+		Assert.assertEquals("key-2012", keys.get(0));
+	}
 
+	@Test
+	public void shouldIncludeBothYears() throws ChronosException {
+		PartitionPeriod period = PartitionPeriod.YEAR;
+
+		Calendar calendar = Calendar.getInstance();
+		calendar.set(2012, 11, 29, 0, 0, 0);
+		calendar.set(Calendar.MILLISECOND, 0);
+		Date d1 = calendar.getTime();
+		calendar.add(Calendar.DATE, 15);
+		Date d2 = calendar.getTime();
+		List<String> keys = period.getPeriodKeys("key", d1, d2);
+
+		Assert.assertEquals(2, keys.size());
+		Assert.assertEquals("key-2012", keys.get(0));
+		Assert.assertEquals("key-2013", keys.get(1));
+	}
+
+	@Test
+	public void shouldInclude3Years() throws ChronosException {
+		PartitionPeriod period = PartitionPeriod.YEAR;
+
+		Calendar calendar = Calendar.getInstance();
+		calendar.set(2012, 11, 29, 0, 0, 0);
+		calendar.set(Calendar.MILLISECOND, 0);
+		Date d1 = calendar.getTime();
+		calendar.add(Calendar.DATE, 15 + 365);
+		Date d2 = calendar.getTime();
+		List<String> keys = period.getPeriodKeys("key", d1, d2);
+
+		Assert.assertEquals(3, keys.size());
+		Assert.assertEquals("key-2012", keys.get(0));
+		Assert.assertEquals("key-2013", keys.get(1));
+		Assert.assertEquals("key-2014", keys.get(2));
+	}
+
+	@Test
+	public void shouldInclude3Months() throws ChronosException {
+		PartitionPeriod period = PartitionPeriod.MONTH;
+
+		Calendar calendar = Calendar.getInstance();
+		calendar.set(2012, 11, 29, 0, 0, 0);
+		calendar.set(Calendar.MILLISECOND, 0);
+		Date d1 = calendar.getTime();
+		calendar.add(Calendar.DATE, 45);
+		Date d2 = calendar.getTime();
+		List<String> keys = period.getPeriodKeys("key", d1, d2);
+
+		Assert.assertEquals(3, keys.size());
+		Assert.assertEquals("key-2012-12", keys.get(0));
+		Assert.assertEquals("key-2013-01", keys.get(1));
+		Assert.assertEquals("key-2013-02", keys.get(2));
+	}
+
+	@Test
+	public void shouldHitProperChronicle() throws ChronosException {
+		PartitionPeriod period = PartitionPeriod.YEAR;
+
+		Calendar calendar = Calendar.getInstance();
+		calendar.set(2012, 11, 29, 0, 0, 0);
+		calendar.set(Calendar.MILLISECOND, 0);
+		long start = calendar.getTimeInMillis();
+		calendar.add(Calendar.DATE, 15);
+		long end = calendar.getTimeInMillis();
+
+		TestChronicle chron = new TestChronicle("test", period);
+
+		long time = start;
+		while (time < end) {
+			chron.add(new ChronologicalRecord(time, "x".getBytes()));
+			time += new Duration("1m").getMillis();
+		}
+
+		Assert.assertEquals(2, chron.chronicles.size());
+		Assert.assertNotNull(chron.chronicles.get("test-2012"));
+		Assert.assertNotNull(chron.chronicles.get("test-2013"));
+		Assert.assertEquals(1440 * 3, chron.chronicles.get("test-2012").size());
+		Assert.assertEquals(1440 * 12, chron.chronicles.get("test-2013").size());
+		Assert.assertEquals(1440 * 3, chron.chronicles.get("test-2012").getNumEvents(start, end));
+		Assert.assertEquals(1440 * 12, chron.chronicles.get("test-2013").getNumEvents(start, end));
+		Assert.assertEquals(1440 * 15, chron.getNumEvents(start, end));
+		Assert.assertEquals(1440 * 15, Iterators.size(chron.getRange(start, end)));
+	}
+
+	private static class TestChronicle extends PartitionedChronicle {
+
+		public final Map<String, MemoryChronicle> chronicles;
+
+		public TestChronicle(String keyPrefix, PartitionPeriod period) {
+			super(keyPrefix, period);
+			chronicles = new HashMap<String, MemoryChronicle>();
+		}
+
+		@Override
+		public Chronicle getPartition(String key) {
+			if (!chronicles.containsKey(key)) {
+				chronicles.put(key, new MemoryChronicle());
+			}
+			return chronicles.get(key);
+		}
+
+	}
 }
